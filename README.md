da56fd3b1943ff25a8061e3f9530b69f25b3775a
Henry Hund <henryhund@gmail.com>
2013-03-17 23:31:15 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# [Lorious](http://lorious.com)
+
+A new app. A side project I am working on. Description to come
+
+by [Henry Hund](http://henryhund.com)
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lorious::Application.config.secret_token = '54fd01ce4e9bf13a263f6e77f53a9c5475f7d0b3bcb934756f9e41c94f7156099c8f4326ceb9e4d60ac1862fec4ee36d98d3fd5dcbf95b03d6392bd1a664d0e0'
